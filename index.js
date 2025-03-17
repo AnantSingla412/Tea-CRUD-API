249c7693f38fb1f841a85585daf5d9a91e359e03
Anant Singla <anantsingla412@gmail.com>
2025-03-17 15:42:36 +0530

    implimented Logger

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.port || 3000;
@@ -7,7 +9,23 @@ app.use(express.json());
 
 let teaData = [];
 let nextId = 1;
+const morganFormat = ":method :url :status :response-time ms";
 
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 //add a new tea
 app.post("/teas", (req, res) => {
   const { name, price } = req.body;
